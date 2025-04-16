67d5af09c296795ab919b50f12c53baaa127d003
ShouldSleep8hr <pearanuban@gmail.com>
2025-04-16 09:13:37 +0700

    change icon to be url

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -7,6 +7,12 @@ const props = defineProps({
 })
 
 const card = new URL('../assets/Cards/Card_Sil_Active.svg', import.meta.url).href
+
+const career = new URL('../assets/Icons/SVG/Icon_Career.svg', import.meta.url).href
+const money = new URL('../assets/Icons/SVG/Icon_Money.svg', import.meta.url).href
+const health = new URL('../assets/Icons/SVG/Icon_Health.svg', import.meta.url).href
+const relationship = new URL('../assets/Icons/SVG/Icon_Relationship.svg', import.meta.url).href
+
 </script>
 
 <template>
@@ -20,26 +26,26 @@ const card = new URL('../assets/Cards/Card_Sil_Active.svg', import.meta.url).hre
       <div class="flex flex-row justify-between items-center w-[90%]">
         <!-- Career -->
         <div class="flex flex-col items-center gap-2 flex-1">
-          <img src="../assets/Icons/SVG/Icon_Career.svg" alt="Career" class="w-[40%] h-auto" />
+          <img :src="career" alt="Career" class="w-[40%] h-auto" />
           <span class="text-black font-semibold text-xs text-center">{{ text_career }}</span>
         </div>
 
         <!-- Money -->
         <div class="flex flex-col items-center gap-2 flex-1">
-          <img src="../assets/Icons/SVG/Icon_Money.svg" alt="Money" class="w-[40%] h-auto" />
+          <img :src="money" alt="Money" class="w-[40%] h-auto" />
           <span class="text-black font-semibold text-xs text-center">{{ text_money }}</span>
         </div>
 
         <!-- Health -->
         <div class="flex flex-col items-center gap-2 flex-1">
-          <img src="../assets/Icons/SVG/Icon_Health.svg" alt="Health" class="w-[40%] h-auto" />
+          <img :src="health" alt="Health" class="w-[40%] h-auto" />
           <span class="text-black font-semibold text-xs text-center">{{ text_health }}</span>
         </div>
 
         <!-- Relationship -->
         <div class="flex flex-col items-center gap-2 flex-1">
           <img
-            src="../assets/Icons/SVG/Icon_Relationship.svg"
+            :src="relationship"
             alt="Relationship"
             class="w-[40%] h-auto"
           />
